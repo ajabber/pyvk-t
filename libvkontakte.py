1e527b24167f0984c5da784e91b33de95b224132
Equidamoid <Equidamoid@localhost>
2009-02-18 04:48:12 +0000

    Initial upload

---
+++ libvkontakte.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+import urllib2
+import urllib
+import cookielib
+import threading
+import time
+from htmlentitydefs import name2codepoint
+from cookielib import Cookie
+from urllib import urlencode
+from BeautifulSoup import BeautifulSoup
+import demjson
+
+
+class vkonClient:
+    def feedChanged(self,jid,feed):
+        print feed
+    def usersOffline(self,jid,users):
+        print "online",users
+    def usersOnline(self,jid,users):
+        print "offline",users
+    def threadError(self,jid,message=""):
+        print "error: %s"%message
+
+class vkonThread(threading.Thread):
+    oldFeed=""
+    onlineList=[]
+    alive=1
+    error=0
+    def __init__(self,cli,jid,email,passw):
+        threading.Thread.__init__(self,target=self.loop)
+        global opener
+        self.jid=jid
+        cjar=cookielib.FileCookieJar()
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cjar))
+        cjar.clear()
+        authData={'email':email, 'pass':passw}
+        params=urllib.urlencode(authData)
+        req=urllib2.Request("http://vkontakte.ru/login.php?%s"%params)
+        req.addheaders = [('User-agent', 'Mozilla/5.0')]
+
+        res=self.opener.open(req)
+        #print cjar
+        self.cookie=cjar.make_cookies(res,req)
+        self.client=cli
+        self.feedOnly=1
+        f=self.getFeed()
+        if (f["user"]["id"]==-1):
+            self.error=1
+            self.client.threadError(self.jid,"auth error (possible wrong email/pawssword)")
+        else:
+            self.error=0
+        #print res.read()
+        #print this.cookie
+    
+    def logout(self):
+        req=urllib2.Request("http://vkontakte.ru/login.php?op=logout")
+        req.addheaders = [('User-agent', 'Mozilla/5.0')]
+        res=self.opener.open(req)
+        print "logout"
+    def getFeed(self):
+        #global opener
+        req=urllib2.Request("http://vkontakte.ru/feed2.php?mask=ufmepvnoq")
+        res=self.opener.open(req)
+        s=res.read().decode("cp1251")
+        #print repr(s)
+        ret=demjson.decode(s)
+        return ret
+
+    def getOnlineList(self):
+        req=urllib2.Request("http://vkontakte.ru/friend.php?act=online&nr=1")
+        res=self.opener.open(req)
+        page=res.read()
+        bs=BeautifulSoup(page)
+        trgDiv=bs.find(name="div",id="searchResults")
+        if (trgDiv==None):
+            return list()
+        trgScr=trgDiv.findAll(name="script")[1].string[14:]
+        a=demjson.decode(trgScr)
+        ret=list()
+        for t in a["list"]:
+            ret.append(t[0])
+        return ret
+    def getInfo(self,v_id):
+        prs=vcardPrs()
+        req=urllib2.Request("http://pda.vkontakte.ru/id%s"%v_id)
+        res=self.opener.open(req)
+        page=res.read()
+        prs.feed(page)
+        return prs.vcard
+    def getVcard(self,v_id):
+        req=urllib2.Request("http://vkontakte.ru/id%s"%v_id)
+        res=self.opener.open(req)
+        page=res.read()
+        bs=BeautifulSoup(page,convertEntities="html",smartQuotesTo="html",fromEncoding="cp-1251")
+        prof=bs.find(name="div", id="userProfile")
+        rc=prof.find(name="div", id="rightColumn")
+        fn=rc.find(name="h2").string.encode("utf-8")
+        return {"fn":fn}
+        
+        print "name",fn
+        ptr=rc.find(name="table", attrs= {"class":"profileTable"}).findAll("tr")
+        for i in ptr:
+            title=i.td.string
+            dat=i.contents[3].div
+            #print title
+            #print dat
+            if (title==u"День рождения:"):
+                cnt=dat.findAll("a")
+                #print cnt
+                #if (len(cnt)==2):
+                    #h=cnt[0]["href"][15:]
+                    #print h
+                    #day=
+                    #print cnt[1].string
+                #print dat
+                #yr=dat.
+        #print pt
+        
+        #TODO name parse
+        return {"fn":fn}
+    def getMessage_old(self,msgid):
+        prs=msgPrs()
+        
+        req=urllib2.Request("http://pda.vkontakte.ru/letter%s"%msgid)
+        res=self.opener.open(req)
+        page=res.read()
+        prs.feed(page)
+        t=prs.msg
+        if(prs.msg["text"][-5:]=="\n\n\t\t\t"):
+            prs.msg["text"]=prs.msg["text"][:-5]
+        #t['text']=t['text']
+        return prs.msg
+    def getMessage(self,msgid):
+        req=urllib2.Request("http://pda.vkontakte.ru/letter%s"%msgid)
+        res=self.opener.open(req)
+        page=res.read()
+        bs=BeautifulSoup(page,convertEntities="html",smartQuotesTo="html")
+        trgForm=bs.find(name="form", action="/mailsent?pda=1")
+        fromField=trgForm.find(name="input",attrs={"name":"to_id"})
+        from_id=fromField["value"]
+        #print "from",repr(fromField)
+        strings=trgForm.findAll(text=lambda (x):x!=u'\n'and x!=' ',recursive=False)
+        title=strings[1]
+        body=""
+        for i in range(2,len(strings)):
+            body=body+"\n"+strings[i]
+        
+        body=body[5:]
+        #print body
+        return {"text":body,"from":from_id,"title":title}
+        #print strings
+    def sendMessage(self,to_id,body,title="[null]"):
+        #prs=chasGetter()
+        req=urllib2.Request("http://pda.vkontakte.ru/?act=write&to=%s"%to_id)
+        res=self.opener.open(req)
+        page=res.read()
+        bs=BeautifulSoup(page,convertEntities="html",smartQuotesTo="html")
+        chas=bs.find(name="input",attrs={"name":"chas"})["value"]
+        print "chas",chas
+        #prs.feed(page)
+        if (type(body)==unicode):
+            tbody=body.encode("utf-8")
+        else:
+            tbody=body
+        if (type(title)==unicode):
+            ttitle=title.encode("utf-8")
+        else:
+            ttitle=title
+
+        data={"to_id":to_id,"title":ttitle,"message":tbody,"chas":chas,"to_reply":0}
+        
+        #print "data: ",urlencode(data)
+        req=urllib2.Request("http://pda.vkontakte.ru/mailsent?pda=1",urlencode(data))
+        try:
+            res=self.opener.open(req)
+            return 1
+        except urllib2.HTTPError:
+            return 0
+        #print res.read()
+    def getFriendList(self):
+        req=urllib2.Request("http://vkontakte.ru/friend.php?nr=1")
+        res=self.opener.open(req)
+        page=res.read()
+        bs=BeautifulSoup(page)
+        trgDiv=bs.find(name="div",id="searchResults")
+        trgScr=trgDiv.findAll(name="script")[1].string[14:]
+        #print trgScr
+        a=demjson.decode(trgScr)
+        #print (a)
+        ret=list()
+        for t in a["list"]:
+            ret.append(t[0])
+        return ret
+    def loop(self):
+        while(self.alive):
+            tfeed=self.getFeed()
+            if (tfeed!=self.oldFeed):
+                self.oldFeed=tfeed
+                self.client.feedChanged(self.jid,tfeed)
+            if (self.feedOnly):
+                tonline=()
+            else:
+                tonline=self.getOnlineList()
+            #print tonline,self.onlineList
+            if (tonline!=self.onlineList):
+                self.client.usersOnline(self.jid,filter(lambda x:self.onlineList.count(x)-1,tonline))
+                self.client.usersOffline(self.jid,filter(lambda x:tonline.count(x)-1,self.onlineList))
+                self.onlineList=tonline
+            time.sleep(10)
+            
+    def exit(self):
+        self.client.usersOffline(self.jid,self.onlineList)
+        self.logout()
+        self.alive=0
+        #threading.Thread.exit(self)
+    def __del__(self):
+        self.logout()
+        threading.Thread.exit(self)
---
+++ pyvk-t.py
@@ -0,0 +1,329 @@
+# -*- coding: utf-8 -*-
+from  pyxmpp.jabberd.component import Component
+import pyxmpp
+import sys
+import logging
+from pyxmpp.all import JID,Iq,Presence,Message,StreamError
+from pyvk_t_db import pyvk_t_db
+from libvkontakte import *
+class transp (Component,vkonClient):
+    db=pyvk_t_db()
+    test=""
+    threads={}
+    def __init__(self, jid=None, secret=None, server=None, port=5347, disco_name=u'PyXMPP based component', disco_category=u'x-service', disco_type=u'x-unknown', keepalive=0):
+        Component.__init__(self, jid, secret, server, port, disco_name, disco_category, disco_type, keepalive)
+        self.disco_info.add_feature("jabber:iq:register")
+        self.disco_info.add_feature("jabber:iq:gateway")
+#        self.disco_info.add_feature("http://jabber.org/protocol/commands")
+        
+    def idle(self,stream):
+        stream=self.get_stream() 
+        if stream: 
+            stream.idle() 
+    def getRegister(self, iq):
+        print "wtf?? register attempt?"
+        to=iq.get_to()
+        if to and to!=self.jid:
+            raise FeatureNotImplementedProtocolError, "Tried to register at non-null node"
+        iq=iq.make_result_response()
+        q=iq.new_query("jabber:iq:register")
+        q.newTextChild(q.ns(),"instructions","Введи свое мыло и пароль на вконтакте.ру.")
+        q.newChild(q.ns(),"email",None)
+        q.newChild(q.ns(),"password",None)
+        self.stream.send(iq)
+        return True
+    def setRegister(self,iq):
+        to=iq.get_to()
+        if to and to!=self.jid:
+            raise FeatureNotImplementedProtocolError, "Tried to register at non-null node"
+        remove=iq.xpath_eval("r:query/r:remove",{"r":"jabber:iq:register"})
+        if remove:
+            m=Message(from_jid=iq.get_to(),to_jid=iq.get_from(),stanza_type="chat",
+                    body=u"Unregistered")
+            self.stream.send(m)
+            p=Presence(from_jid=iq.get_to(),to_jid=iq.get_from(),stanza_type="unsubscribe")
+            self.stream.send(p)
+            p=Presence(from_jid=iq.get_to(),to_jid=iq.get_from(),stanza_type="unsubscribed")
+            self.stream.send(p)
+            return True
+        username=iq.xpath_eval("r:query/r:email",{"r":"jabber:iq:register"})
+        if username:
+            username=username[0].getContent()
+        else:
+            username=u""
+        password=iq.xpath_eval("r:query/r:password",{"r":"jabber:iq:register"})
+        if password:
+            password=password[0].getContent()
+        else:
+            password=u""
+        self.db.addUser(iq.get_from().bare().as_unicode(),username,password)
+        self.db.sync()
+        m=Message(from_jid=iq.get_to(),to_jid=iq.get_from(),stanza_type="chat",body=u"Регистрация произведена.")
+        self.stream.send(m)
+        p=Presence(from_jid=iq.get_to(),to_jid=iq.get_from(),stanza_type="subscribe")
+        self.stream.send(p)
+        iq=iq.make_result_response()
+        self.stream.send(iq)
+        return True
+    def presence(self,stanza):
+        jid=stanza.get_from().bare()
+        ud=self.db.userData(jid.as_unicode())
+        #FIXME use pyxmpp's jid instead of unicode string
+        if (ud):
+            print repr(stanza.get_type())
+            if (stanza.get_type()==u"unavailable"):
+                p=Presence(stanza_type=u"unavailable" ,to_jid=stanza.get_from(), from_jid=self.jid, show=stanza.get_show());
+                self.stream.send(p)
+                if (self.threads.has_key(jid)):
+                    print "killing thread"
+                    self.threads[jid].exit()
+                    del self.threads[jid]
+            else:
+                
+                if (stanza.get_type()==u"probe"):
+                    pass
+                    #self.startSession(jid,1)
+                else:
+                    p=Presence(stanza_type=u"available" ,to_jid=stanza.get_from(), from_jid=self.jid, show=stanza.get_show());
+                    self.stream.send(p)
+                    
+                    self.startSession(jid,0)
+                
+        return True
+    def usersOffline(self,jid,users):
+        for u in users:
+            tjid=pyxmpp.jid.JID.__new__(pyxmpp.jid.JID,domain=self.jid.domain,node_or_jid=str(u))
+            p=Presence(
+            stanza_type="unavailable",
+            to_jid=jid,
+            from_jid=tjid,
+            status="http://vkontakte.ru/id%s"%u
+            );
+            self.stream.send(p)
+        return True
+    def usersOnline(self,jid,users):
+        for u in users:
+            tjid=pyxmpp.jid.JID.__new__(pyxmpp.jid.JID,domain=self.jid.domain,node_or_jid=str(u))
+            p=Presence(
+                stanza_type=None,
+                to_jid=jid,
+                from_jid=tjid
+            );
+            self.stream.send(p)
+        return True
+    def presenceControl(self,stanza):
+        """Handle subscription control <presence/> stanzas -- acknowledge
+        them."""
+        p=stanza.make_accept_response()
+        self.stream.send(p)
+        return True
+    def msgHandler(self,msg):
+        txt=msg.get_body()
+        jid=msg.get_from().bare()
+        to_jid=msg.get_to()
+        ud=self.db.userData(jid.as_unicode())
+        if (ud):
+            if (to_jid!=self.jid):
+                if (self.threads.has_key(jid)):
+                    if (self.threads[jid].sendMessage(to_id=to_jid.node,body=txt,title="[sent by pyvk-t]")==0):
+                        m=Message(
+                            from_jid=self.jid,
+                            to_jid=jid,
+                            stanza_type="chat",
+                            body=u"Ошибка отправки сообщения. Скорее всего, вконтакт потребовал капчу. Попробуй убрать ссылки из сообщения."
+                        )
+                        self.stream.send(m)
+                        
+
+            if (txt=="get roster"):
+                m=Message(
+                    from_jid=self.jid,
+                    to_jid=jid,
+                    stanza_type="chat",
+                    body=u"""Запрос списка друзей сопровождается большим количеством запросов авторизации.
+Рекомендуется временно включить автоматическую авторизацию в клиенте.
+Для подтверждения отправь 'get roster confirm'"""
+                );
+                self.stream.send(m)
+            elif (txt=="get roster confirm"):
+                if (self.threads.has_key(jid)):
+                    rost=self.threads[jid].getFriendList()
+                    m=Message(
+                    from_jid=self.jid,
+                    to_jid=jid,
+                    stanza_type="chat",
+                    body=u"Получаю список друзей... это может занять некоторое время..."
+                    );
+                    self.stream.send(m)
+                    for i in rost:
+                        newJid=pyxmpp.jid.JID.__new__(pyxmpp.jid.JID,node_or_jid="%s"%i,domain=self.jid.domain)
+                        print repr(newJid)
+                        p=Presence(from_jid=newJid,to_jid=msg.get_from(),stanza_type="subscribe")
+                        self.stream.send(p)
+                        p=Presence(from_jid=newJid,to_jid=msg.get_from(),stanza_type="subscribed")
+                        self.stream.send(p)
+                else:
+                    #TODO err message
+                    pass
+                
+            pass
+        else:
+            m=Message(
+            from_jid=self.jid,
+            to_jid=jid,
+            stanza_type="chat",
+            body=u"u r not registered, sorry"
+            );
+            self.stream.send(m)
+            
+            
+        return True
+    def getVcard(self,iq):
+        #print "vcard"
+        #print repr(iq.get_to())
+        jid=iq.get_from().bare()
+        if (iq.get_to()!=self.jid):
+            if (self.threads.has_key(jid)):
+                iq=iq.make_result_response()
+                q=iq.new_query("vcard-temp","vCard")
+                vc=self.threads[jid].getVcard(int(iq.get_from().node))
+                #print vc
+                q.newTextChild(q.ns(),"FN",vc["fn"])
+                self.stream.send(iq)
+        pass
+    def startSession(self,jid,feedOnly=1):
+        ud=self.db.userData(jid.as_unicode())
+        if (ud):
+            pass
+        else:
+            return 0
+        if (self.threads.has_key(jid)):
+            self.threads[jid].feedOnly=feedOnly
+        else:
+            p=Presence(
+                stanza_type=None,
+                to_jid=jid,
+                from_jid=self.jid,
+                show="away",
+                status="logging in..."
+            );
+            self.stream.send(p)
+            self.threads[jid]=vkonThread(cli=self,jid=jid,email=ud["email"],passw=ud["password"])
+            #print "test"
+            if (self.threads[jid].error):
+                self.threads[jid].exit()
+                p=Presence(
+                stanza_type="unavailable",
+                to_jid=jid,
+                from_jid=self.jid,
+                status="login error"
+                );
+                self.stream.send(p)
+                return 0
+            else:
+                self.threads[jid].start()
+                self.threads[jid].feedOnly=feedOnly
+                return 1
+                    
+    def setCommand(self,cmd):
+        print "set command"
+    def getCommand(self,cmd):
+        print "get command"
+        q=cmd.get_query()
+        #print str(q)
+        for p in q.properties:
+            print "name: ",p.name
+            print "val:",p
+        #print p
+        #print p[0]
+        
+    def authenticated(self):
+        Component.authenticated(self)
+        self.stream.set_iq_get_handler("query","jabber:iq:register",self.getRegister)
+        self.stream.set_iq_set_handler("query","jabber:iq:register",self.setRegister)
+        self.stream.set_message_handler("chat",self.msgHandler,priority=0)
+        #self.stream.set_iq_get_handler("query","http://jabber.org/protocol/disco#items",self.getCommand)
+        #self.stream.set_iq_set_handler("query","http://jabber.org/protocol/disco#items",self.setCommand)
+        self.stream.set_iq_get_handler("vCard","vcard-temp",self.getVcard)
+        
+        self.stream.set_presence_handler("available",self.presence)
+        self.stream.set_presence_handler("probe",self.presence)
+        
+        self.stream.set_presence_handler("unavailable",self.presence)
+        self.stream.set_presence_handler("subscribe",self.presenceControl)
+        self.stream.set_presence_handler("unsubscribe",self.presenceControl)
+        pass
+    def feedChanged(self,jid,feed):
+        dat=[]
+        dat=feed
+        ret=""
+        #print jid
+        for k in dat.keys():
+            if (k!="user" and dat[k]["count"]):
+                ret=ret+"new %s: %s\n"%(k,dat[k]["count"])
+        if (feed["messages"]["count"] ):
+            
+            for i in feed ["messages"]["items"].keys():
+                if (self.threads[jid].feedOnly):
+                    m=Message(from_jid=sejf.jid,to_jid=jid,stanza_type="chat",body="you have new message(s)")
+                    self.stream.send(m)
+                    pass
+                else:
+                    msg=self.threads[jid].getMessage(i)
+                    tjid=pyxmpp.jid.JID.__new__(pyxmpp.jid.JID,node_or_jid="%s"%msg["from"],domain=self.jid.domain)
+                    m=Message(from_jid=tjid,to_jid=jid,stanza_type="chat",body="[%s]\n%s"%(msg["title"],msg['text']))
+                    self.stream.send(m)
+        if (self.threads[jid].feedOnly):
+            ret="[shadow mode]\n%s"%ret
+        p=Presence(
+            stanza_type=None,
+            to_jid=jid,
+            from_jid=self.jid,
+            status=ret
+        );
+        self.stream.send(p)
+    def threadError(self,jid,message):
+        m=Message(
+        from_jid=self.jid,
+        to_jid=jid,
+        stanza_type="chat",
+        body=u"Ошибка: %s"%message
+        );
+        self.stream.send(m)
+        if (self.threads.has_key(jid)):
+            self.threads[jid].exit()
+            p=Presence(
+                stanza_type="unavailable",
+                to_jid=u,
+                from_jid=self.jid,
+                status="system error. u can try to reconnect."
+            );
+            self.stream.send(p)
+    def disconnect(self):
+        print "logging out..."
+        for u in self.threads.keys():
+            self.threads[u].exit()
+            p=Presence(
+                stanza_type="unavailable",
+                to_jid=u,
+                from_jid=self.jid,
+                status="transport shutdown..."
+            );
+            self.stream.send(p)
+        print "done"
+        Component.disconnect(self)
+
+logger=logging.getLogger()
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+jid=pyxmpp.jid.JID.__new__(pyxmpp.jid.JID,domain='vkontakte.eqx.su')
+tr=transp(jid=jid,secret="passfortrans",server="eqx.su",port=5348,disco_name=u'Vkontakte.ru Transport [pre-alpha]',keepalive=100)
+tr.connect()
+try:
+    tr.loop(1)
+except KeyboardInterrupt:
+    print "disconnecting..."
+    tr.disconnect()
+    pass
+    
