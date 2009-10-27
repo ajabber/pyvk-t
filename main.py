da63a52aa7bb2105827e2a230f53c66d0f00f757
Equidamoid <Equidamoid@localhost>
2009-10-27 19:08:14 +0000

    * ???...

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ if(os.environ.has_key("PYVKT_CONFIG")):
     
 op=optparse.OptionParser()
 op.add_option('-c','--config',default=confName,help='configuration file name')
-op.add_option('-a','--admin-only',action='store_false', default=True, help='only admin can use transport when this flag is enabled')
+op.add_option('-a','--admin-only',action='store_true', default=False, help='only admin can use transport when this flag is enabled')
 op.add_option('-l','--autologin', action='store_true', default=False)
 
 opt,args=op.parse_args()
-conf.read(opt['config'])
+conf.read(opt.config)
 lvl=logging.WARNING
 if ("--debug" in sys.argv):
     lvl=logging.DEBUG
@@ -25,9 +25,9 @@ logging.basicConfig(level=lvl,format='  *  %(asctime)s [%(levelname)s] %(message
 s=pyvkt.component.pyvk_t(conf.get("general","jid"))
 s.connect(conf.get("general","server"),conf.get("general","port"),conf.get("general","secret"))
 logging.warn("connected")
-if (opt['autologin']):
+if (opt.autologin):
     s.addResource(conf.get("general","admin"))
-if (opt['admin_only']):
+if (opt.admin_only):
     print "isActive=0"
     s.isActive=0
 s.startPoll()
