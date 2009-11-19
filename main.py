badc899787b1670a0935683b10227aa023f70d46
mroztn <mroztn@localhost>
2009-11-19 19:08:21 +0000

    UPD:   * ??????? ???????????? ?????????? --logmode ??? -m     ??? ?????? ????????? --logmode debug

--- main.py
+++ main.py
@@ -4,20 +4,29 @@ import logging,os,sys
 import pyvkt.component
 import pyvkt.config as conf
 import optparse
+
 confName="pyvkt.cfg"
 if(os.environ.has_key("PYVKT_CONFIG")):
     confName=os.environ["PYVKT_CONFIG"]
     
 op=optparse.OptionParser()
-op.add_option('-c','--config',default=confName,help='configuration file name')
+op.add_option('-c','--config',default=confName, help='configuration file name')
 op.add_option('-d','--admin-only',action='store_true', default=False, help='only admin can use transport when this flag is enabled')
 op.add_option('-a','--autologin', action='store_true', default=False)
-op.add_option('-l','--logfile', default=None,help='log file name')
+op.add_option('-l','--logfile', default=None, help='log file name')
+op.add_option('-m','--logmode', default="warning", help='Log mode')
 
 opt,args=op.parse_args()
 
+LOG_LEVELS={'debug': logging.DEBUG,
+            'info': logging.INFO,
+            'warning': logging.WARNING,
+            'error': logging.ERROR,
+            'critical': logging.CRITICAL}
+lvl=LOG_LEVELS.get(opt.logmode.lower(), logging.NOTSET)
+
 LOG_FORMAT='  *  %(asctime)s [%(levelname)3.3s] %(funcName)s[%(threadName)s]: %(message)s'
-logging.basicConfig(level=logging.WARNING,format=LOG_FORMAT)
+logging.basicConfig(level=lvl,format=LOG_FORMAT)
 if (opt.logfile):
     import logging.handlers
     logging.warning('writing log to %s'%opt.logfile)
